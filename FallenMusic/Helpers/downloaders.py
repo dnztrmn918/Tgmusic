1d30a603131085ae176cf8fd7a975cfd08de0d7c
dnztrmn918 <dnztrmnn@icloud.com>
2025-07-30 16:33:58 +0300

    Update downloaders.py
    Signed-off-by: dnztrmn918 <dnztrmnn@icloud.com>

--- FallenMusic/Helpers/downloaders.py
+++ FallenMusic/Helpers/downloaders.py
@@ -1,31 +1,29 @@
 import os
 from yt_dlp import YoutubeDL
 
-# Dosyanın kendi dizinini alıp, bir üst klasördeki cookies.txt'yi buluyoruz
-cookie_path = os.path.join(os.path.dirname(__file__), "..", "cookies.txt")
-
-ydl_opts = {
-    "format": "bestaudio/best",
-    "outtmpl": "downloads/%(id)s.%(ext)s",
-    "geo_bypass": True,
-    "nocheckcertificate": True,
-    "quiet": True,
-    "no_warnings": True,
-    "prefer_ffmpeg": True,
-    "cookiefile": cookie_path,  # Buraya cookies.txt dosyasının yolu eklendi
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "320",
-        }
-    ],
-}
-
-ydl = YoutubeDL(ydl_opts)
-
-
 def audio_dl(url: str) -> str:
+    cookie_path = os.path.join(os.path.dirname(__file__), "..", "cookies.txt")
+    
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": "downloads/%(id)s.%(ext)s",
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "quiet": True,
+        "no_warnings": True,
+        "prefer_ffmpeg": True,
+        "cookiefile": cookie_path,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "320",
+            }
+        ],
+    }
+    
+    ydl = YoutubeDL(ydl_opts)
+    
     sin = ydl.extract_info(url, False)
     x_file = os.path.join("downloads", f"{sin['id']}.mp3")
     if os.path.exists(x_file):
